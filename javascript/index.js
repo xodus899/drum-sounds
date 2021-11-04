81214267d2147129850caf4be9613e2beff8a30a
Christopher Justice <19810192+xodus899@users.noreply.github.com>
2021-11-04 11:16:21 -0400

    Update case for uppercase keys

--- javascript/index.js
+++ javascript/index.js
@@ -6,31 +6,31 @@ function buttonClick() {
     document.querySelectorAll('.drum')[i].addEventListener('click', function() {
       let buttonInnerHTML = this.innerHTML;
       switch (buttonInnerHTML) {
-        case "w":
+        case 'w':
           let tom1 = new Audio('sounds/tom-1.mp3');
           tom1.play();
           break;
-        case "s":
+        case 's':
           let tom2 = new Audio('sounds/tom-2.mp3');
           tom2.play();
           break;
-        case "a":
+        case 'a':
           let tom3 = new Audio('sounds/tom-3.mp3');
           tom3.play();
           break;
-        case "d":
+        case 'd':
           let tom4 = new Audio('sounds/tom-4.mp3');
           tom4.play();
           break;
-        case "j":
+        case 'j':
           let snare = new Audio('sounds/snare.mp3');
           snare.play();
           break;
-        case "k":
+        case 'k':
           let crash = new Audio('sounds/crash.mp3');
           crash.play();
           break;
-        case "l":
+        case 'l':
           let kick = new Audio('sounds/kick-bass.mp3');
           kick.play();
           break;
@@ -47,30 +47,37 @@ function keyPress() {
   document.addEventListener('keydown', (keyPressed) => {
     switch (keyPressed.key) {
       case 'w':
+      case 'W':
         let tom1 = new Audio('sounds/tom-1.mp3');
         tom1.play();
         break;
       case 's':
+      case 'S':
         let tom2 = new Audio('sounds/tom-2.mp3');
         tom2.play();
         break;
       case 'a':
+      case 'A':
         let tom3 = new Audio('sounds/tom-3.mp3');
         tom3.play();
         break;
       case 'd':
+      case 'D':
         let tom4 = new Audio('sounds/tom-4.mp3');
         tom4.play();
         break;
       case 'j':
+      case 'J':
         let snare = new Audio('sounds/snare.mp3');
         snare.play();
         break;
       case 'k':
+      case 'K':
         let crash = new Audio('sounds/crash.mp3');
         crash.play();
         break;
       case 'l':
+      case 'L':
         let kick = new Audio('sounds/kick-bass.mp3');
         kick.play();
         break;
